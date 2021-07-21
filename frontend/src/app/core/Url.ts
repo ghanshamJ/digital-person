98bc99042a7e67657e97b67927be3a72ad161849
ghansham <ghansham@klaycapital.in>
2021-07-21 13:38:11 +0530

    changed Urls

--- frontend/src/app/core/Url.ts
+++ frontend/src/app/core/Url.ts
@@ -3,9 +3,9 @@ import { Injectable } from "@angular/core";
   providedIn: "root",
 })
 export class URL {
-  readonly authURL = "http://localhost:3000/api/v1/auth";
+  readonly authURL = "https://person-apk.herokuapp.com/api/v1/auth";
   readonly authURL2 = "https://person-apk.herokuapp.com/auth";
-  readonly addPersonURL = "http://localhost:3000/api/v1/person";
-  readonly getPersonsURL = "http://localhost:3000/api/v1/persons";
-  readonly getPersonCountByCountryURL = "http://localhost:3000/api/v1/statistics-by-country";
+  readonly addPersonURL = "https://person-apk.herokuapp.com/api/v1/person";
+  readonly getPersonsURL = "https://person-apk.herokuapp.com/api/v1/persons";
+  readonly getPersonCountByCountryURL = "https://person-apk.herokuapp.com/api/v1/statistics-by-country";
 }
